697710859f9c31451f2eb3bf1a2fba235b200787
Kamil Żabiński <kamil.adam.zabinski@gmail.com>
2018-03-13 11:48:29 +0100

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+alias srcsh='source ~/.bashrc'
+alias vish='vim ~/.bashrc'
+alias ..='cd ..'
+
+alias sbtit='sbt clean coverage scalafmt::test test:scalafmt::test it:scalafmt::test test it:test coverageReport'
+alias sbti='sbt clean coverage scalafmt test:scalafmt it:scalafmt test it:test coverageReport'
+alias sbttt='sbt clean coverage scalafmt::test test:scalafmt::test test coverageReport'
+alias sbtt='sbt clean coverage scalafmt test:scalafmt test coverageReport'
+alias style='sbt scalastyle'
+
+alias dsbtit='./sbt clean coverage scalafmt::test test:scalafmt::test it:scalafmt::test test it:test coverageReport'
+alias dsbti='./sbt clean coverage scalafmt test:scalafmt it:scalafmt test it:test coverageReport'
+alias dsbttt='./sbt clean coverage scalafmt::test test:scalafmt::test test coverageReport'
+alias dsbtt='./sbt clean coverage scalafmt test:scalafmt test coverageReport'
+alias dstyle='./sbt scalastyle'
