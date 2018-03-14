54b14022d44196cb58711f8ef8e7d1ec1a9dea81
Kamil Żabiński <kamil.adam.zabinski@gmail.com>
2018-03-14 09:14:42 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,9 @@ alias srcsh='source ~/.bashrc'
 alias vish='vim ~/.bashrc'
 alias ..='cd ..'
 
+alias g='git'
+alias gfix='g rebase -i HEAD~2; g push -f'
+
 alias sbtit='sbt clean coverage scalafmt::test test:scalafmt::test it:scalafmt::test test it:test coverageReport'
 alias sbti='sbt clean coverage scalafmt test:scalafmt it:scalafmt test it:test coverageReport'
 alias sbttt='sbt clean coverage scalafmt::test test:scalafmt::test test coverageReport'
