ca29925baa1da9b45581707196a581043a7d3bbd
Kamil Żabiński <kamil.adam.zabinski@gmail.com>
2018-04-04 13:35:39 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -31,3 +31,10 @@ alias dsbtt='./sbt clean sbt:scalafmt scalafmt test:scalafmt coverage test cover
 alias dsbti="./sbt clean sbt:scalafmt scalafmt test:scalafmt it:scalafmt coverage test it:test coverageReport && sbt coverageAggregate"
 alias dsbtia="./sbt clean sbt:scalafmt scalafmt test:scalafmt it:scalafmt coverage test it:test assembly coverageReport && sbt coverageAggregate"
 
+alias dsbtit_='./sbt clean scalafmt::test test:scalafmt::test it:scalafmt::test coverage test it:test coverageReport'
+alias dsbtt_="./sbt clean scalafmt test:scalafmt coverage test assembly coverageReport && sbt coverageAggregate"
+alias dsbttia_="./sbt clean scalafmt test:scalafmt coverage test coverageReport && sbt coverageAggregate"
+alias dsbtt_='./sbt clean scalafmt test:scalafmt coverage test coverageReport'
+alias dsbti_="./sbt clean scalafmt test:scalafmt it:scalafmt coverage test it:test coverageReport && sbt coverageAggregate"
+alias dsbtia_="./sbt clean scalafmt test:scalafmt it:scalafmt coverage test it:test assembly coverageReport && sbt coverageAggregate"
+
