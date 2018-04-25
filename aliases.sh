6f0444c09c265d3528e9bf0e76fcd0d63283aeb9
Kamil Żabiński <kamil.adam.zabinski@gmail.com>
2018-04-25 11:24:47 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -9,39 +9,7 @@ alias gfix2='g stash; g fix; g stash pop'
 alias vish="vim ~/.bashrc"
 alias srcsh="source ~/.bashrc"
 
-
-# OLD
-
-alias style='sbt scalastyle'
-alias sbtss="sbt stats scalastyle"
-
-alias sbttt='sbt clean sbt:scalafmt::test scalafmt::test test:scalafmt::test coverage test coverageReport'
-alias sbtt="sbt clean sbt:scalafmt scalafmt test:scalafmt coverage test coverageReport && sbt coverageAggregate"
-alias sbtta="sbt clean sbt:scalafmt scalafmt test:scalafmt coverage test assembly coverageReport && sbt coverageAggregate"
-alias sbtit='sbt clean sbt:scalafmt::test scalafmt::test test:scalafmt::test it:scalafmt::test coverage test it:test coverageReport'
-alias sbti="sbt clean sbt:scalafmt scalafmt test:scalafmt it:scalafmt coverage test it:test coverageReport && sbt coverageAggregate"
-alias sbtia="sbt clean scalafmt test:scalafmt it:scalafmt coverage test it:test assembly coverageReport && sbt coverageAggregate"
-
-alias dstylegc='./sbt scalastyleGenerateConfig'
-alias dstyle='./sbt scalastyle'
-alias dsbtss="./sbt stats scalastyle"
-
-alias dsbtit='./sbt clean sbt:scalafmt::test scalafmt::test test:scalafmt::test it:scalafmt::test coverage test it:test coverageReport'
-alias dsbtt="./sbt clean sbt:scalafmt scalafmt test:scalafmt coverage test assembly coverageReport && sbt coverageAggregate"
-alias dsbttia="./sbt clean sbt:scalafmt scalafmt test:scalafmt coverage test coverageReport && sbt coverageAggregate"
-alias dsbtt='./sbt clean sbt:scalafmt scalafmt test:scalafmt coverage test coverageReport'
-alias dsbti="./sbt clean sbt:scalafmt scalafmt test:scalafmt it:scalafmt coverage test it:test coverageReport && sbt coverageAggregate"
-alias dsbtia="./sbt clean sbt:scalafmt scalafmt test:scalafmt it:scalafmt coverage test it:test assembly coverageReport && sbt coverageAggregate"
-
-alias dsbtit_='./sbt clean scalafmt::test test:scalafmt::test it:scalafmt::test coverage test it:test coverageReport'
-alias dsbtt_="./sbt clean scalafmt test:scalafmt coverage test assembly coverageReport && sbt coverageAggregate"
-alias dsbttia_="./sbt clean scalafmt test:scalafmt coverage test coverageReport && sbt coverageAggregate"
-alias dsbtt_='./sbt clean scalafmt test:scalafmt coverage test coverageReport'
-alias dsbti_="./sbt clean scalafmt test:scalafmt it:scalafmt coverage test it:test coverageReport && sbt coverageAggregate"
-alias dsbtia_="./sbt clean scalafmt test:scalafmt it:scalafmt coverage test it:test assembly coverageReport && sbt coverageAggregate"
-
-# NEW
-
+# SBT
 alias dsbt='./sbt'
 alias dsbtsgc='dsbt scalastyleGenerateConfig'
 alias dsbta='dsbt assembly' 
@@ -60,5 +28,3 @@ alias dsbtfmt_="dsbtcl scalafmt test:scalafmt it:scalafmt"
 # dynamic
 alias dsbtdi="dsbt coverage test it:test coverageReport"
 alias dsbtdu="dsbt coverage test coverageReport"
-
-
